dbc1c390b9130756eb2ec90975c5e4676de018e3
ayush-that <ayush1337@hotmail.com>
2025-12-07 22:07:22 +0530

    add: bg image to sign-in and sign-up pages

--- apps/web/src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import SignInViewPage from "@/features/auth/components/sign-in-view";
 
 export const metadata: Metadata = {
-  title: "Authentication | Sign In",
-  description: "Sign In page for authentication.",
+  title: "Bohemia AI | Sign In",
+  description: "Sign In page for Bohemia AI.",
 };
 
 export default function Page() {
--- apps/web/src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ apps/web/src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import SignUpViewPage from "@/features/auth/components/sign-up-view";
 
 export const metadata: Metadata = {
-  title: "Authentication | Sign Up",
-  description: "Sign Up page for authentication.",
+  title: "Bohemia AI | Sign Up",
+  description: "Sign Up page for Bohemia AI.",
 };
 
 export default function Page() {
